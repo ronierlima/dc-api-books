cc07a7a7ecf679d755974d4a91308d4c7aa90173
Ronier Lima <ronier.lim@gmail.com>
2022-09-17 13:28:43 -0300

    Subindo o projeto

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => {
+  try {
+    const pagina = +req.query.page || 1;
+    const limite = +req.query.limit || 50;
+
+    const totalPages = Math.ceil(books.length / limite);
+
+    if (pagina <= totalPages) {
+      const booksFilter = books.slice(
+        pagina * limite - limite,
+        pagina * limite
+      );
+
+      res.status(200).json({
+        currentPage: 0,
+        totalElements: books.length,
+        totalPages: totalPages,
+        content: booksFilter,
+      });
+    }
+  } catch (err) {
+    res.status(500);
+  }
+});
+
+app.listen(9000, () => {
+  console.log(`Starting Server on Port ${port}`);
+});
--- src/routes/booksRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/booksController')
-
-router.get('/', controller.get);
-router.get('/:id', controller.getById);
-
-module.exports = router;
